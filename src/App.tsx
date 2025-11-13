9f87a74a36ddc403174464468b1693d26ea8e5b0
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-11-13 11:36:18 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-background">
+        <h1 className="text-foreground font-roboto">Hello</h1>
+        <h1 className="text-muted font-sans">Hello</h1>
+        <h1 className="text-shadow-secondary font-sans">Market discovery</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
